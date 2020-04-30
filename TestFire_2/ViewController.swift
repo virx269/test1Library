75687e3525ca90033c84e99f2acd5ef28973e4f7
VirX <virx269@gmail.com>
2020-04-30 16:11:08 +0300

    Add files via upload

---
+++ TestFire_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFire_2
+//
+//  Created by  Виталий on 30.04.2020.
+//  Copyright © 2020 Bulavin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
